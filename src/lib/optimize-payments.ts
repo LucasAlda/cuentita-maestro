3bb6aef6c932619dd90dfb0d1f1a0b8bd4782955
Lucas Aldazabal <lucas@aldazabal.com.ar>
2024-06-17 19:54:09 -0300

    fix algorithm 8

--- src/lib/optimize-payments.ts
+++ src/lib/optimize-payments.ts
@@ -6,12 +6,12 @@ export function optimizePayments(
   balances.sort((a, b) => a.balance - b.balance);
 
   let i = 0;
-  while (balances[0].balance <= -0.01 && i < balances.length * 5) {
+  while (balances[0].balance < -0.01 && i < balances.length * 5) {
     const from = balances[0];
     const to = balances.at(-1)!;
     const amount = Number(Math.min(-from.balance, to.balance).toFixed(2));
 
-    if (amount < 0.01) return;
+    if (amount < 0.01) break;
 
     payments.push({
       fromId: from.id,
